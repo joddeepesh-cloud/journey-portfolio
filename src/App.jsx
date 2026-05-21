e0dcca71029eac30ba0ab8f538116a19ef01708c
joddeepesh-cloud <joddeepesh@gmail.com>
2026-05-21 23:23:04 +0530

    Added 3D car scene and cyber city setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,71 @@ import Car from "./components/Car"
 function Ground() {
   return (
     <>
-      <mesh rotation={[-Math.PI/2,0,0]} position={[0,-1,0]}>
-        <planeGeometry args={[50,50]} />
-        <meshStandardMaterial color="green" />
+      {/* Ground */}
+      <mesh rotation={[-Math.PI/2,0,0]} position={[0,-2,0]}>
+        <planeGeometry args={[80,80]} />
+        <meshStandardMaterial color="#111"/>
       </mesh>
 
-      <mesh rotation={[-Math.PI/2,0,0]} position={[0,-0.9,0]}>
-        <planeGeometry args={[6,50]} />
-        <meshStandardMaterial color="gray" />
+      {/* Road */}
+      <mesh rotation={[-Math.PI/2,0,0]} position={[0,-1.9,0]}>
+        <planeGeometry args={[10,80]} />
+        <meshStandardMaterial color="#333"/>
+      </mesh>
+
+      {/* Road line */}
+      <mesh rotation={[-Math.PI/2,0,0]} position={[0,-1.8,0]}>
+        <planeGeometry args={[0.3,80]} />
+        <meshStandardMaterial color="white"/>
       </mesh>
     </>
   )
 }
 
+function Building({x,z,height,color}) {
+  return (
+    <mesh position={[x,height/2-2,z]}>
+      <boxGeometry args={[3,height,3]} />
+      <meshStandardMaterial
+        color={color}
+        emissive={color}
+        emissiveIntensity={0.5}
+      />
+    </mesh>
+  )
+}
+
 export default function App() {
   return (
-    <Canvas camera={{ position:[0,4,15] }}>
-      <ambientLight intensity={1.5}/>
-      <directionalLight position={[5,5,5]}/>
+    <Canvas camera={{position:[0,3,10],fov:75}}>
 
-      <Ground />
-      <Car />
+      <ambientLight intensity={1}/>
 
-      <OrbitControls />
+      <pointLight
+        position={[5,5,5]}
+        intensity={10}
+        color="cyan"
+      />
+
+      <pointLight
+        position={[-5,5,5]}
+        intensity={10}
+        color="purple"
+      />
+
+      <Ground/>
+
+      <Car/>
+
+      {/* Buildings */}
+      <Building x={-8} z={-5} height={6} color="cyan"/>
+      <Building x={8} z={-5} height={8} color="purple"/>
+
+      <Building x={-8} z={5} height={5} color="pink"/>
+      <Building x={8} z={5} height={10} color="blue"/>
+
+      <OrbitControls/>
+
     </Canvas>
   )
 }
--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -1,15 +1,41 @@
-import { useGLTF } from "@react-three/drei"
-
 function Car() {
-  const car = useGLTF("/models/car.glb")
-
   return (
-    <primitive
-      object={car.scene}
-      scale={6}
-      position={[0,-0.8,0]}
-      rotation={[0, Math.PI, 0]}
-    />
+    <group position={[0,0,0]} scale={2}>
+
+      {/* Body */}
+      <mesh position={[0,0,0]}>
+        <boxGeometry args={[4,1,2]} />
+        <meshStandardMaterial color="cyan" />
+      </mesh>
+
+      {/* Top */}
+      <mesh position={[0,1,0]}>
+        <boxGeometry args={[2.5,1,2]} />
+        <meshStandardMaterial color="cyan" />
+      </mesh>
+
+      {/* Wheels */}
+      <mesh position={[-1.5,-0.7,1]}>
+        <sphereGeometry args={[0.5]} />
+        <meshStandardMaterial color="black" />
+      </mesh>
+
+      <mesh position={[1.5,-0.7,1]}>
+        <sphereGeometry args={[0.5]} />
+        <meshStandardMaterial color="black" />
+      </mesh>
+
+      <mesh position={[-1.5,-0.7,-1]}>
+        <sphereGeometry args={[0.5]} />
+        <meshStandardMaterial color="black" />
+      </mesh>
+
+      <mesh position={[1.5,-0.7,-1]}>
+        <sphereGeometry args={[0.5]} />
+        <meshStandardMaterial color="black" />
+      </mesh>
+
+    </group>
   )
 }
 
